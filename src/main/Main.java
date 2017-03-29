1d3c6f77e4d44f737fa41be393a24af576e50bb2
Christian <bouwensec@gmail.com>
2017-03-29 00:11:09 -0400

    Able to parse the PDF in order

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,29 @@
 package main;
 
+import java.io.File;
+import java.io.IOException;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.pdfparser.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		PDDocument doc = new PDDocument();
-		System.out.print("Hello World!");
+		try {
+		    String text = getText(new File("C:\\Users\\Christian\\Google Drive\\Jobs\\Arnold\\sheriff_foreclosuresales_list.pdf"));
+		    System.out.println("Text in PDF: " + text);
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 		
+	}
+	
+	static String getText(File pdfFile) throws IOException {
+	    PDDocument doc = PDDocument.load(pdfFile);
+	    PDFTextStripper stripper = new PDFTextStripper();
+	    stripper.setSortByPosition(true);
+	    return stripper.getText(doc);
 	}
 
 }
